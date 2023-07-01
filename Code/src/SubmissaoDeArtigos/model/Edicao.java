f80ac1786e51b77f58d53b63c1a19298f07f93ba
jpbuenos <119616181+jpbuenos@users.noreply.github.com>
2023-07-01 19:35:32 -0300

    Update Edicao.java

--- Code/src/SubmissaoDeArtigos/model/Edicao.java
+++ Code/src/SubmissaoDeArtigos/model/Edicao.java
@@ -1,11 +1,39 @@
-
 package SubmissaoDeArtigos.model;
 
 public class Edicao {
     private int id;
     private Revista local;
     private String numEdicao;
-    // Outros atributos e métodos
-    
-    // Construtores, getters e setters
+
+    // Construtor
+    public Edicao(int id, Revista local, String numEdicao) {
+        this.id = id;
+        this.local = local;
+        this.numEdicao = numEdicao;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Revista getLocal() {
+        return local;
+    }
+
+    public void setLocal(Revista local) {
+        this.local = local;
+    }
+
+    public String getNumEdicao() {
+        return numEdicao;
+    }
+
+    public void setNumEdicao(String numEdicao) {
+        this.numEdicao = numEdicao;
+    }
 }
