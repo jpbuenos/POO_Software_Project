ea4c855f654f9c6b08c479254abee9ebe0076fca
jpbuenos <119616181+jpbuenos@users.noreply.github.com>
2023-07-01 19:41:01 -0300

    Update Revista.java

--- Code/src/SubmissaoDeArtigos/model/Revista.java
+++ Code/src/SubmissaoDeArtigos/model/Revista.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package SubmissaoDeArtigos.model;
 
 public class Revista {
@@ -9,7 +5,55 @@ public class Revista {
     private String nome;
     private Pessoa editorChefe;
     private int ISSN;
-    // Outros atributos e métodos
-    
-    // Construtores, getters e setters
+
+    // Construtor
+    public Revista(int id, String nome, Pessoa editorChefe, int ISSN) {
+        this.id = id;
+        this.nome = nome;
+        this.editorChefe = editorChefe;
+        this.ISSN = ISSN;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Pessoa getEditorChefe() {
+        return editorChefe;
+    }
+
+    public void setEditorChefe(Pessoa editorChefe) {
+        this.editorChefe = editorChefe;
+    }
+
+    public int getISSN() {
+        return ISSN;
+    }
+
+    public void setISSN(int ISSN) {
+        this.ISSN = ISSN;
+    }
 }
+
+Pessoa editorChefe = new Pessoa("Nome do Editor Chefe");
+Revista revista = new Revista(1, "Nome da Revista", editorChefe, 123456789);
+
+System.out.println(revista.getNome());
+System.out.println(revista.getEditorChefe().getNome()); 
+System.out.println(revista.getISSN());
+
+revista.setId(2);
+System.out.println(revista.getId()); 
